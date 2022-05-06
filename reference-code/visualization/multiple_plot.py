61dda1b396cf22ae68dc1f3b058621e04d7da11a
이동균 <85166445+Dong-gyun-lee@users.noreply.github.com>
2022-05-06 20:44:02 +0900

    Update multiple_plot.py

--- reference-code/visualization/multiple_plot.py
+++ reference-code/visualization/multiple_plot.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize=(16,8))
 ax = fig.add_subplot()
-ax.plot(t1.시간,t1.승차인원수,marker='o',label='plot1')
-ax.plot(t2.시간,t2.승차인원수,marker='o',label='plot2')
+ax.plot(x,y,marker='o',label='plot1')
+ax.plot(x,y,marker='o',label='plot2')
 ax.legend(fontsize=20)
 plt.xlabel('y축',fontsize=15)
 plt.ylabel('x축',fontsize=15)
